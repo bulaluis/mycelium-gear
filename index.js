832466fdee5fcb49539cd161dfc3dacadfa0ff47
lf94 <ircsurfer33@gmail.com>
2017-01-10 12:48:07 -0500

    Separate Gateway and Order classes

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const fetch      = require('node-fetch')
 const XSignature = require('./x-signature')
 
 /*
-    A Mycelium Gear order class to handle creating orders.
+    A Mycelium Gear Gateway class to handle creating gateways to use with orders.
 */
-let Order = function(gatewayId, secret) {
-    this.gatewayId = gatewayId
-    this.secret    = secret
+let Gateway = function(id, secret) {
+    this.id     = id
+    this.secret = secret
+}
+
+/*
+    A Mycelium Gear Order class that prepares an order to be sent to a gateway.
+    Prepare an order, but don't send it to Mycelium Gear yet.
+*/
+let Order = function (gateway, amountInSatoshi, callbackData) {
+    this.gateway = gateway
+    this.domain  = 'https://gateway.gear.mycelium.com'
+    this.uri     = `/gateways/${this.gateway.id}/orders?`
+    + `amount=${amountInSatoshi}`
+    + `&keychain_id=${this.keychainId}`
+    + `&callback_data=${callbackData}`
+
+    this.url    = this.domain + this.uri
+    this.method = 'POST'
+
+    this.prepared = true
 }
 
 /*
@@ -20,7 +38,7 @@ Order.prototype.send = function() {
     if(! this.prepared) { return Promise.reject('Order is not prepared!'); }
 
     let headers = {}
-    Object.assign(headers, XSignature.sign(_this.secret, _this.method, _this.uri, '').toHeaders())
+    Object.assign(headers, XSignature.sign(_this.gateway.secret, _this.method, _this.uri, '').toHeaders())
 
     const orderFulfillmentRequest = fetch(this.url, {
         method: _this.method,
@@ -37,25 +55,6 @@ Order.prototype.send = function() {
     return orderFulfillmentRequest;
 }
 
-/*
-    Prepare an order, but don't send it to Mycelium Gear yet.
-*/
-Order.prototype.prepare = function (amountInSatoshi, callbackData) {
-
-    this.domain = 'https://gateway.gear.mycelium.com'
-    this.uri    = `/gateways/${this.gatewayId}/orders?`
-    + `amount=${amountInSatoshi}`
-    + `&keychain_id=${this.keychainId}`
-    + `&callback_data=${callbackData}`
-
-    this.url    = this.domain + this.uri
-    this.method = 'POST'
-
-    this.prepared = true
-
-    return this;
-}
-
 Order.prototype.getNextKeychainId = function() {
     const _this = this
     const url   = `https://gateway.gear.mycelium.com/gateways/${this.gatewayId}/last_keychain_id`
@@ -68,5 +67,6 @@ Order.prototype.getNextKeychainId = function() {
 }
 
 module.exports = {
+    Gateway,
     Order
 }
